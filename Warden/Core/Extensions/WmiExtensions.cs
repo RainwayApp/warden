ac3875d99fa657f316b174d38bbf596786343d1d
Andrew Sampson <andrew@rainway.com>
2020-03-12 15:06:36 -0700

    remove unused deps

--- Warden/Core/Extensions/WmiExtensions.cs
+++ Warden/Core/Extensions/WmiExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Warden.Core.Extensions
+﻿namespace Warden.Core.Extensions
 {
     /// <summary>
     /// 
