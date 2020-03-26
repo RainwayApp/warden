a91eea161ac5ba735f933788afa98fb7500ee7d8
Andrew Sampson <andrew@rainway.com>
2020-03-25 17:51:09 -0700

    fix issue caused by weird processes

--- Warden/Core/WardenImpersonator.cs
+++ Warden/Core/WardenImpersonator.cs
@@ -15,14 +15,24 @@ namespace Warden.Core
         private readonly WindowsImpersonationContext _context;
         private readonly WindowsIdentity _identity;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public IntPtr Token => _identity.Token;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public WardenImpersonator()
         {
             _identity = GetIdentity();
             _context = _identity.Impersonate();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public string UserName()
         {
             return WindowsIdentity.GetCurrent().Name;
@@ -39,29 +49,45 @@ namespace Warden.Core
         private static extern bool DuplicateToken(IntPtr existingTokenHandle,
             int securityImpersonationLevel, ref IntPtr duplicateTokenHandle);
 
-        private const int TokenDuplicate = 2;
-        private const int TokenQuery = 0X00000008;
-        private const int TokenImpersonate = 0X00000004;
+        private const int TOKEN_TOKEN_DUPLICATE = 2;
+        private const int TOKEN_TOKEN_QUERY = 0X00000008;
+        private const int TOKEN_TOKEN_IMPERSONATE = 0X00000004;
+
+
+        private IntPtr GetExplorerHandle()
+        {
+            foreach (var p in Process.GetProcesses())
+            {
+                try
+                {
+                    if (!p.ProcessName.Equals("explorer")) continue;
+                    var handle = p.Handle;
+                    return handle;
+                }
+                catch
+                {
+                    // Ignore forbidden processes so we can get a list of processes we do have access to
+                }
+            } 
+            return IntPtr.Zero;
+        }
 
         private WindowsIdentity GetIdentity()
         {
             var hToken = IntPtr.Zero;
-            var explorer = Process.GetProcessesByName("explorer").FirstOrDefault();
+            var explorer = GetExplorerHandle();
 
-            if (explorer == null)
+            if (explorer == IntPtr.Zero)
             {
                 return null;
             }
-            if (OpenProcessToken(explorer.Handle,
-                    TokenQuery | TokenImpersonate | TokenDuplicate,
-                    ref hToken) != 0)
+            if (OpenProcessToken(explorer, TOKEN_TOKEN_QUERY | TOKEN_TOKEN_IMPERSONATE | TOKEN_TOKEN_DUPLICATE, ref hToken) != 0)
             {
                 var newId = new WindowsIdentity(hToken);
                 try
                 {
                     const int securityImpersonation = 2;
-                    var dupeTokenHandle = DupeToken(hToken,
-                        securityImpersonation);
+                    var dupeTokenHandle = DupeToken(hToken, securityImpersonation);
                     if (IntPtr.Zero == dupeTokenHandle)
                     {
                         var s = $"Token duplication failed {Marshal.GetLastWin32Error()}, privilege not held";
